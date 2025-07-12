5016d6b93efc41f90abf50e80cbc8dc25735a674
Mescxll <202411190002@ifba.edu.br>
2025-07-11 23:37:28 -0300

    Adicionando interface

---
+++ src/Arborizavel.java
@@ -0,0 +1,13 @@
+public interface Arborizavel<Disciplina>{
+    void inserirDisciplina(Disciplina nova);
+    void removerDisciplina(String codigo);
+
+    Nodo buscarNodo(String codigo);
+
+    String exibirArvore(Nodo atual, int nivel);
+    String visualizarArvore();
+
+    boolean contemDisciplina(String codigo);
+    boolean removerNodo(String codigo, Nodo atual, Nodo pai);
+    boolean vincularPreRequisito(String codigoPai, String codigoFilho);   
+}
