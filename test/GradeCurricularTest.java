8f7746c00bcca7fc5e282cdfbdedb9af1944ff1e
Vinícius <viniciusandradebc@gmail.com>
2025-07-18 09:14:12 -0300

    Adicionando testes de Buscar Nodo

--- test/GradeCurricularTest.java
+++ test/GradeCurricularTest.java
@@ -1,18 +1,31 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
-import java.util.List;
 import org.junit.Before;
 
 public class GradeCurricularTest {
     private GradeCurricular<Disciplina> grade;
+    private Disciplina disciplina;
 
     @Before
     public void setUp() {
         grade = new GradeCurricular<>();
+        disciplina = new Disciplina("Matemática Discreta I", 0);
     }
 
     @Test
-    public void buscarNodoTest() {
+    public void buscarNodoPorNomeTest() {
+        grade.inserirDisciplina(disciplina);
+        assertEquals(disciplina, grade.buscarNodo("Matemática Discreta I").getDado());
+    }
 
+    @Test
+    public void buscarNodoPorCodigoTest() {
+        grade.inserirDisciplina(disciplina);
+        assertEquals(disciplina, grade.buscarNodo(1).getDado());
+    }
+
+    @Test
+    public void buscarNodoVazioTest() {
+        assertNull(grade.buscarNodo(1));
     }
 }
